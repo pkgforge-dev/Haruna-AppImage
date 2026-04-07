c994a4307db5b794e829a8ca4a6b2fd4ea34e6c4
Link4Electronics <link4electronics@gmail.com>
2026-04-07 00:51:27 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,11 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm haruna
+pacman -Syu --noconfirm \
+    kvantum       \
+    lxqt-qtplugin \
+    haruna        \
+    qt6ct
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
