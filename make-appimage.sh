2e35b81f2517fe2fdee2de724d5c85f5644dd4f7
Link4Electronics <link4electronics@gmail.com>
2026-04-11 18:01:03 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 VERSION=$(pacman -Q haruna | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
-export ADD_HOOKS="self-updater.bg.hook:get-yt-dlp.hook"
+export ADD_HOOKS="self-updater.hook:get-yt-dlp.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/haruna.png
 export DESKTOP=/usr/share/applications/org.kde.haruna.desktop
