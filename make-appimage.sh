562b2eb601d231a698aa67d858795c4064558bc9
Link4Electronics <link4electronics@gmail.com>
2026-08-30 06:08:21 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,8 +10,6 @@ export ADD_HOOKS="self-updater.hook:get-yt-dlp.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/256x256/apps/haruna.png
 export DESKTOP=/usr/share/applications/org.kde.haruna.desktop
-export DEPLOY_QT=1
-export QT_DIR=qt6
 export DEPLOY_VULKAN=1
 export DEPLOY_PIPEWIRE=1
 
@@ -20,3 +18,5 @@ quick-sharun /usr/bin/haruna
 
 # Turn AppDir into AppImage
 quick-sharun --make-appimage
+
+export USE_HOST_DRIVERS_EXPERIMENTAL=1
